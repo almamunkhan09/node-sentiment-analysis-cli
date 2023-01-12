6959924752156c309a8908a40191fe83c98bb9b6
Al Mamun Khan <almamunkhan09@gmail.com>
2023-01-12 19:26:27 +0100

    Initiate require files

---
+++ index.js
@@ -0,0 +1,8 @@
+// import dotenv and initiate to use
+import * as dotenv from 'dotenv';
+// import moneklearn for the sentiment analysis API
+import monkeylearn from 'monkeylearn';
+
+dotenv.config();
+
+console.log('hello');
